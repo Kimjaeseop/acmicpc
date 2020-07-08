5b12088fd37bf49dcc5c0ee1b636e37b62baa1b4
Jaeseop <jaeseb96@naver.com>
2020-07-08 14:26:29 +0900

    2805.07.08

--- 2805/2805.cpp
+++ 2805/2805.cpp
@@ -20,7 +20,7 @@ int main(void)
 	for (int i = 0; i < N; i++) {
 		scanf("%d", &in);
 
-		if (in > r)
+		if (in > r) // 최대값 지정
 			r = in;
 
 		V.push_back(in);
@@ -34,14 +34,14 @@ int main(void)
 
 		S = V.size();
 		for (int i = 0; i < S; i++) {
-			sum += (V[i] - mid < 0 ? 0 : V[i] - mid);
+			sum += (V[i] - mid < 0 ? 0 : V[i] - mid); // 집에 들고가는 나무가 양수이면 합을 구함
 		}
 
-		if (sum == M) {
+		if (sum == M) { // 정확하게 같은 경우
 			MAX = mid;
 			break;
 		}
-		else if (sum < M) {
+		else if (sum < M) { // 합이 필요한 나무의 길이보다 작다면 최대 크기를 조정 // 덜 잘라야 함 // 미드 값이 작아져야 함
 			r = mid - 1;
 		}
 		else if (sum >= M) {
