81c54d24993dc9964078363ad7b6be5503ea6c52
jaeseb96@naver.com <jaeseb96@naver.com>
2021-03-31 16:56:30 +0900

    2021.03.31 V0.3

--- 15649/15649.py
+++ 15649/15649.py
@@ -1,19 +1,18 @@
 N, M = map(int, input().split())
 
 def func(depth, lst):
-	if depth == M-1:
-		print(*lst)
-		return
+    if depth == M-1:
+        print(*lst)
+        return
 
-	for i in range(1, N+1):
-		if i not in lst:
-			lst[depth+1] = i
-			func(depth+1, lst)
-			lst[depth+1] = 0
+    for i in range(1, N+1):
+        if i not in lst:
+            lst[depth+1] = i # 다음 depth 노드에 값 대입
+            func(depth+1, lst)
+            lst[depth+1] = 0 # 이미 자식노드에 방문하고 부모노드로 다시 올라왔기 때문에 노드에 값 제거
 
 for i in range(1, N+1):
-	arr = [0 for _ in range(1, M+1)]
+    arr = [0 for _ in range(1, M+1)]
 
-	arr[0] = i
-	func(0, arr)
-
+    arr[0] = i 
+    func(0, arr)
